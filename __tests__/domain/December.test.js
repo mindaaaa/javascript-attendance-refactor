8a88862e486ccb95910865338c9a87ffbe1098c9
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-12-15 15:51:56 +0900

    feat(December): 12월 날짜에 휴일 여부 포함 객체 구현

--- __tests__/domain/December.test.js
+++ __tests__/domain/December.test.js
@@ -9,5 +9,20 @@ describe('December 클래스 단위테스트', () => {
     // then
     expect(december.days).toHaveLength(31);
     console.log(new Date().toLocaleString('ko-KR'));
+    console.log(new Date().getDay());
+  });
+
+  test('December 인스턴스로부터 얻은 달력 정보에는 공휴일 여부가 포함되어 있다.', () => {
+    // given
+    const december = new December();
+
+    // when
+    const days = december.days;
+    const result = days.every((day) => {
+      return Reflect.has(day, 'isHoliday');
+    });
+
+    // then
+    expect(result).toBe(true);
   });
 });
--- src/domain/December.js
+++ src/domain/December.js
@@ -1,10 +1,21 @@
 class December {
   #days;
-
+  #Holidays = [25];
   constructor() {
     this.#days = [];
     for (let i = 1; i <= 31; i++) {
-      const day = new Date(2024, 11, i);
+      const date = new Date(2024, 11, i);
+      const isHoliday = date.getDay() === 0 || date.getDay() === 6;
+      const day = {
+        date,
+        isHoliday,
+        stringified: date.toLocaleString('ko-KR'),
+      };
+
+      if (!day.isHoliday && this.#Holidays.includes(day.date.getDate())) {
+        day.isHoliday = true;
+      }
+
       this.#days.push(day);
     }
   }
