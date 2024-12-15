29c50b2d01427d67bbb584907332d4204f008107
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-12-15 16:32:49 +0900

    test(December): setEntry 테스트 추가

--- __tests__/domain/December.test.js
+++ __tests__/domain/December.test.js
@@ -1,11 +1,15 @@
 import December from '../../src/domain/December.js';
 
 describe('December 클래스 단위테스트', () => {
+  let december = null;
+
+  beforeEach(() => {
+    december = new December();
+  });
+
   test('December 클래스를 인스턴스화 했을 경우 12월 달력 정보를 확인할 수 있어야 한다.', () => {
     // given
     // when
-    const december = new December();
-
     // then
     expect(december.days).toHaveLength(31);
     console.log(new Date().toLocaleString('ko-KR'));
@@ -14,16 +18,42 @@ describe('December 클래스 단위테스트', () => {
 
   test('December 인스턴스로부터 얻은 달력 정보에는 공휴일 여부가 포함되어 있다.', () => {
     // given
-    const december = new December();
-
     // when
     const days = december.days;
-    // const result = days.every((day) => {
-    //   return Reflect.has(day, 'isHoliday');
-    // });
 
     // then
-    days.map((day) => expect(day).toHaveProperty('isHoliday'));
-    //expect(result).toBe(true);
+    days.forEach((day) => expect(day).toHaveProperty('isHoliday'));
+  });
+
+  test('December 인스턴스로부터 얻은 달력 정보에는 기본적으로 (결석)으로 설정된 entry 정보가 포함되어 있다.', () => {
+    // given
+    // when
+    const days = december.days;
+
+    // then
+    days.forEach((day) =>
+      expect(day.entry).toStrictEqual({
+        hour: null,
+        minute: null,
+        status: '(결석)',
+      })
+    );
+  });
+
+  test('setEntry() 메소드는 day 인자에 전달된 날짜의 출결 상태를 업데이트한다.', () => {
+    // given
+    const expectedDay = 16;
+    const entry = [10, 5, '(출석)'];
+
+    // when
+    december.setEntry(expectedDay, entry);
+
+    // then
+    const target = december.days.find(
+      (day) => day.date.getDate() === expectedDay
+    );
+    expect(target.entry).toHaveProperty('hour', entry[0]);
+    expect(target.entry).toHaveProperty('minute', entry[1]);
+    expect(target.entry).toHaveProperty('status', entry[2]);
   });
 });
