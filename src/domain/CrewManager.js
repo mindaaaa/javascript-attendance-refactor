d49a2416871d11f074d32ddc3f721fd24fccb74b
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-12-15 17:09:14 +0900

    feat(CrewManager): csv 파일을 처리하는 CrewManager 구현

--- src/domain/CrewManager.js
+++ src/domain/CrewManager.js
@@ -1,3 +1,36 @@
-class CrewManager {}
+import Crew from './Crew.js';
 
-export default CrewManager();
+class CrewManager {
+  #crews;
+
+  constructor(data) {
+    // data[0] = { nickname, datetime }
+    const nameSet = data.reduce((set, curr) => {
+      set.add(curr.nickname);
+
+      return set;
+    }, new Set());
+
+    const crews = Array.from(nameSet).map((name) => new Crew(name));
+
+    for (const { nickname, datetime } of data) {
+      const { date, hour, minute } = this.#getDatetime(datetime);
+      // TODO: status는 시간에 따라 결정되도록 개선 필요함
+      crews
+        .find((crew) => crew.name === nickname)
+        .setEntry(date, [hour, minute, '(출석)']);
+    }
+
+    this.#crews = crews;
+  }
+
+  #getDatetime(datetime) {
+    return {
+      date: datetime.getDate(),
+      hour: datetime.getHour(),
+      minute: datetime.getMinute(),
+    };
+  }
+}
+
+export default CrewManager;
